6eb0d92cd34f80e1acdd758b11c1675cfc58a614
morshedulmunna <morshedulmunna1@gmail.com>
2023-08-18 21:53:09 +0600

    fix image path

--- main.js
+++ main.js
@@ -1,2 +1,36 @@
 import "./style.scss";
 // main.js
+
+const tabButtons = document.querySelectorAll(".nav_link");
+const tabs = document.querySelectorAll(".tab");
+
+tabButtons.forEach((button) => {
+  button.addEventListener("click", (event) => {
+    event.stopPropagation();
+
+    const tabId = button.getAttribute("data-tab");
+
+    tabs.forEach((tab) => {
+      tab.style.display = "none";
+    });
+
+    const selectedTab = document.getElementById(tabId);
+    selectedTab.style.display = "block";
+
+    tabButtons.forEach((btn) => {
+      btn.classList.remove("active-tab");
+    });
+
+    button.classList.add("active-tab");
+  });
+});
+
+document.addEventListener("click", (event) => {
+  tabs.forEach((tab) => {
+    tab.style.display = "none";
+  });
+
+  tabButtons.forEach((btn) => {
+    btn.classList.remove("active-tab");
+  });
+});
